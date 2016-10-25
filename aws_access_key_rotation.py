23adf17d11f481445b6575b7859240a06acecb91
kernelon <purushothaman.vinod@gmail.com>
2016-10-25 12:17:22 +0000

    Created a function to list IAM users

--- aws_access_key_rotation.py
+++ aws_access_key_rotation.py
@@ -1,3 +1,22 @@
+#import boto3
+import boto3
+
+#import boto3 session
+import boto3.session
+
+#import threading
+import threading
+
+class Aws_Iam(threading.Thread):
+    def list_Iam_Users_With_Access_Key(self):
+	session = boto3.session.Session()
+	iam = session.resource('iam')
+	for users in iam.users.all():
+ 	    print(users)
+
+a = Aws_Iam()
+a.list_Iam_Users_With_Access_Key()
+
 #List all users with access key.
 #Create a second access key in addition to the one in use.
 #Email all users with new access key and the expiry day of the previous access key.
