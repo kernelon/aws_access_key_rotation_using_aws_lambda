86d96de0a782ce5d00bf81925f55dc50a81c8674
kernelon <purushothaman.vinod@gmail.com>
2016-10-25 08:11:35 +0000

    Created a list of requirements

---
+++ aws_access_key_rotation.py
@@ -0,0 +1,7 @@
+#List all users with access key.
+#Create a second access key in addition to the one in use.
+#Email all users with new access key and the expiry day of the previous access key.
+#Update all your applications to use the new access key and validate that the applications are working.
+#Change the state of the previous access key to inactive.
+#Validate that your applications are still working as expected.
+#Delete the inactive access key.
